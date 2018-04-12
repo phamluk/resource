6cad12bd029254166a2e9a41b6d2fd1c67220795
luc <luc.pham@logigear.com>
2018-04-12 14:59:47 +0700

    1

--- task-outputs-to-inputs/create_some_files.sh
+++ task-outputs-to-inputs/create_some_files.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-mkdir some-files1
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
+echo "file1" > some-files1/file1
+echo "file2" > some-files1/file2
+echo "file3" > some-files1/file3
+echo "file4" > some-files1/file4
 
-ls some-files/*
+ls some-files1/*
