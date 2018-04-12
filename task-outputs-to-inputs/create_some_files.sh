66ba05f8fc2725cf2150acc1f017a957af137af1
luc <luc.pham@logigear.com>
2018-04-12 15:02:38 +0700

    asds

--- task-outputs-to-inputs/create_some_files.sh
+++ task-outputs-to-inputs/create_some_files.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+mkdir some-file
 
-echo "file1" > some-files1/file1
-echo "file2" > some-files1/file2
-echo "file3" > some-files1/file3
-echo "file4" > some-files1/file4
+echo "file1" > some-files/file1
+echo "file2" > some-files/file2
+echo "file3" > some-files/file3
+echo "file4" > some-files/file4
 
 ls some-files1/*
+ls some-file/*
